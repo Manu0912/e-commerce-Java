513862492897ee72a342f1af021c73c75480b8b7
nexusram <nahuelsuarez97@hotmail.com>
2021-06-15 18:48:39 -0300

    se crea los metodos removeUser,UserListAdd,getUser,userPosition,toString

--- src/List/UserList.java
+++ src/List/UserList.java
@@ -1,30 +1,79 @@
 package List;
 
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import types_users.Users;
 
-import types_users.Client;
 
-
-public class UserList extends Client {
-private ArrayList<Client> list;
+public class UserList extends Users {
+private HashSet<Users> list;
 
 public UserList() {
-	 list = new ArrayList<Client>();
+	 list = new HashSet<Users>();
 }
+
+public void UserListAdd(Users date) {
+	 list.add(date);
+}
+
+public Users removeUser(String name)
+{
+	int flag=-1;
+	Users us=null;
+	Iterator <Users>iterator= list.iterator();
+	while(iterator.hasNext()&&flag==-1)
+	{
+		if(iterator.next().getName()==name)
+		{
+			flag=0;
+			iterator.remove();
+			us=iterator.next();
+		}
+	}
+	return us;
+}
+public Users getUser(String name)
+{
+	int flag=-1;
+	Users us=null;
+	Iterator <Users>iterator= list.iterator();
+	while(iterator.hasNext()&&flag==-1)
+	{
+		if(iterator.next().getName()==name)
+		{
+			flag=0;
+			us=iterator.next();
+		}
+	}
+	return us;
+}
+
+
 
 public int userPosition(String name)
 {
-	int i=-1;
-	while(list.size()>i)
+	int i=-1,flag=-1;
+	Iterator <Users>iterator= list.iterator();
+	while(iterator.hasNext()&&flag==-1)
 	{
-		if(list.get(i).getName() == name&&i==-1)
+		if(iterator.next().getName()==name)
 		{
-			
+			flag=0;
 		}
 		++i;
 	}
 	return i;
 }
-
+public String toString()
+{
+	int flag=-1;
+	StringBuilder stri= new StringBuilder();
+	Iterator <Users>iterator= list.iterator();
+	while(iterator.hasNext()&&flag==-1)
+	{
+			stri.append(iterator.next().toString());
+	}
+	return stri.toString();
+}
 }
