90bc32bcb3139889978dd6898f350f9e1c273e34
nexusram <nahuelsuarez97@hotmail.com>
2021-06-15 20:07:18 -0300

    arreglo de UserList, paso de HashSet a HasMap

--- src/List/UserList.java
+++ src/List/UserList.java
@@ -1,34 +1,38 @@
 package List;
 
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map.Entry;
+
 import types_users.Users;
 
 
-public class UserList extends Users {
-private HashSet<Users> list;
+public class UserList {
+private HashMap<String,Users> listMapUsers;
 
 public UserList() {
-	 list = new HashSet<Users>();
+	listMapUsers = new HashMap<String,Users>();
 }
 
-public void UserListAdd(Users date) {
-	 list.add(date);
+public void UserListAdd(String category,Users date) {
+	listMapUsers.put(category,date);
 }
 
 public Users removeUser(String name)
 {
 	int flag=-1;
 	Users us=null;
-	Iterator <Users>iterator= list.iterator();
-	while(iterator.hasNext()&&flag==-1)
+	Iterator<Entry<String,Users>> it = listMapUsers.entrySet().iterator();
+	while(it.hasNext()&&flag==-1)
 	{
-		if(iterator.next().getName()==name)
+		Entry<String,Users> entry = it.next();
+		if(entry.getValue().getName()==name)
 		{
 			flag=0;
-			iterator.remove();
-			us=iterator.next();
+			us=entry.getValue();
+			it.remove();
 		}
 	}
 	return us;
@@ -37,43 +41,49 @@ public Users getUser(String name)
 {
 	int flag=-1;
 	Users us=null;
-	Iterator <Users>iterator= list.iterator();
-	while(iterator.hasNext()&&flag==-1)
+	Iterator<Entry<String,Users>> it = listMapUsers.entrySet().iterator();
+	while(it.hasNext()&&flag==-1)
 	{
-		if(iterator.next().getName()==name)
+		Entry<String,Users> entry = it.next();
+		if(entry.getValue().getName()==name)
 		{
 			flag=0;
-			us=iterator.next();
+			us=entry.getValue();
+		}
+	}
+	return us;
+}
+public String getCategory(String name)
+{
+	int flag=-1;
+	String us=null;
+	Iterator<Entry<String,Users>> it = listMapUsers.entrySet().iterator();
+	while(it.hasNext()&&flag==-1)
+	{
+		Entry<String,Users> entry = it.next();
+		if(entry.getValue().getName()==name)
+		{
+			flag=0;
+			us=entry.getKey();
 		}
 	}
 	return us;
 }
 
 
-
-public int userPosition(String name)
-{
-	int i=-1,flag=-1;
-	Iterator <Users>iterator= list.iterator();
-	while(iterator.hasNext()&&flag==-1)
-	{
-		if(iterator.next().getName()==name)
-		{
-			flag=0;
-		}
-		++i;
-	}
-	return i;
-}
-public String toString()
+public String toString(String category)
 {
 	int flag=-1;
-	StringBuilder stri= new StringBuilder();
-	Iterator <Users>iterator= list.iterator();
-	while(iterator.hasNext()&&flag==-1)
-	{
-			stri.append(iterator.next().toString());
+	StringBuilder retorno = new StringBuilder();
+	Iterator<Entry<String,Users>> it = listMapUsers.entrySet().iterator();
+	while (it.hasNext()&&flag==-1) {
+		Entry<String, Users> entry = it.next();
+		if (entry.getKey()== category) {
+			flag=0;
+			retorno.append(entry.getValue().toString()+"\t"+entry.getKey()+"\n");
+		}
 	}
-	return stri.toString();
+	return retorno.toString();
 }
+
 }
