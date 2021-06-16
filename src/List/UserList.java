380ac5ec1b7bc5d0ce34f9e790d30dd8ecda66bb
MatiMorilla <MatiMorilla@users.noreply.github.com>
2021-06-16 14:10:02 -0300

    Revert "Integer id por String category"
    This reverts commit ed7661e973bad94741d32f14886982f9a4cf2608.

--- src/List/UserList.java
+++ src/List/UserList.java
@@ -10,24 +10,24 @@ import types_users.Users;
 
 
 public class UserList {
-private HashMap<Integer,Users> listMapUsers;
+private HashMap<String,Users> listMapUsers;
 
 public UserList() {
-	listMapUsers = new HashMap<Integer,Users>();
+	listMapUsers = new HashMap<String,Users>();
 }
 
-public void UserListAdd(Integer id,Users date) {
-	listMapUsers.put(id,date);
+public void UserListAdd(String category,Users date) {
+	listMapUsers.put(category,date);
 }
 
 public Users removeUser(String name)
 {
 	int flag=-1;
 	Users us=null;
-	Iterator<Entry<Integer,Users>> it = listMapUsers.entrySet().iterator();
+	Iterator<Entry<String,Users>> it = listMapUsers.entrySet().iterator();
 	while(it.hasNext()&&flag==-1)
 	{
-		Entry<Integer,Users> entry = it.next();
+		Entry<String,Users> entry = it.next();
 		if(entry.getValue().getName()==name)
 		{
 			flag=0;
@@ -41,10 +41,10 @@ public Users getUser(String name)
 {
 	int flag=-1;
 	Users us=null;
-	Iterator<Entry<Integer,Users>> it = listMapUsers.entrySet().iterator();
+	Iterator<Entry<String,Users>> it = listMapUsers.entrySet().iterator();
 	while(it.hasNext()&&flag==-1)
 	{
-		Entry<Integer,Users> entry = it.next();
+		Entry<String,Users> entry = it.next();
 		if(entry.getValue().getName()==name)
 		{
 			flag=0;
@@ -53,14 +53,14 @@ public Users getUser(String name)
 	}
 	return us;
 }
-public Integer getId(String name)
+public String getCategory(String name)
 {
 	int flag=-1;
-	int us=-1;
-	Iterator<Entry<Integer,Users>> it = listMapUsers.entrySet().iterator();
+	String us=null;
+	Iterator<Entry<String,Users>> it = listMapUsers.entrySet().iterator();
 	while(it.hasNext()&&flag==-1)
 	{
-		Entry<Integer,Users> entry = it.next();
+		Entry<String,Users> entry = it.next();
 		if(entry.getValue().getName()==name)
 		{
 			flag=0;
@@ -71,14 +71,14 @@ public Integer getId(String name)
 }
 
 
-public String toString(Integer id)
+public String toString(String category)
 {
 	int flag=-1;
 	StringBuilder retorno = new StringBuilder();
-	Iterator<Entry<Integer,Users>> it = listMapUsers.entrySet().iterator();
+	Iterator<Entry<String,Users>> it = listMapUsers.entrySet().iterator();
 	while (it.hasNext()&&flag==-1) {
-		Entry<Integer, Users> entry = it.next();
-		if (entry.getKey()== id) {
+		Entry<String, Users> entry = it.next();
+		if (entry.getKey()== category) {
 			flag=0;
 			retorno.append(entry.getValue().toString()+"\t"+entry.getKey()+"\n");
 		}
