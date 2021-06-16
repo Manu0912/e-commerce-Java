38dfc6e5b533aae5d9c11580759810d58437c975
Manu0912 <emanuelcacheda@hotmail.com>
2021-06-16 20:40:09 -0300

    funcion login agregada

--- src/List/UserList.java
+++ src/List/UserList.java
@@ -1,6 +1,5 @@
 package List;
 
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -8,82 +7,71 @@ import java.util.Map.Entry;
 
 import types_users.Users;
 
-
 public class UserList {
-	private HashMap<Integer,Users> listMapUsers;
-	
+	private HashMap<Integer, Users> listMapUsers;
+
 	public UserList() {
-		listMapUsers = new HashMap<Integer,Users>();
+		listMapUsers = new HashMap<Integer, Users>();
 	}
-	
-	public void UserListAdd(Integer id,Users date) {
-		listMapUsers.put(id,date);
-		// listMapuser.put(date.getId(), date); recomendacion para no tener que pedir el ID
+
+	public void UserListAdd(Integer id, Users date) {
+		listMapUsers.put(id, date);
+		// listMapuser.put(date.getId(), date); recomendacion para no tener que pedir el
+		// ID
 	}
-	
-	public Users removeUser(String name)
-	{
-		int flag=-1;
-		Users us=null;
-		Iterator<Entry<Integer,Users>> it = listMapUsers.entrySet().iterator();
-		while(it.hasNext()&&flag==-1)
-		{
-			Entry<Integer,Users> entry = it.next();
-			if(entry.getValue().getName()==name)
-			{
-				flag=0;
-				us=entry.getValue();
+
+	public Users removeUser(String name) {
+		int flag = -1;
+		Users us = null;
+		Iterator<Entry<Integer, Users>> it = listMapUsers.entrySet().iterator();
+		while (it.hasNext() && flag == -1) {
+			Entry<Integer, Users> entry = it.next();
+			if (entry.getValue().getName() == name) {
+				flag = 0;
+				us = entry.getValue();
 				it.remove();
 			}
 		}
 		return us;
 	}
-	public Users getUser(String name)
-	{
-		int flag=-1;
-		Users us=null;
-		Iterator<Entry<Integer,Users>> it = listMapUsers.entrySet().iterator();
-		while(it.hasNext()&&flag==-1)
-		{
-			Entry<Integer,Users> entry = it.next();
-			if(entry.getValue().getName()==name)
-			{
-				flag=0;
-				us=entry.getValue();
-			}
-		}
-		return us;
-	}
-	
-	
-	public Integer getId(String name)
-	{
-		int flag=-1;
-		int us=-1;
-		Iterator<Entry<Integer,Users>> it = listMapUsers.entrySet().iterator();
-		while(it.hasNext()&&flag==-1)
-		{
-			Entry<Integer,Users> entry = it.next();
-			if(entry.getValue().getName()==name)
-			{
-				flag=0;
-				us=entry.getKey();
-			}
-		}
-		return us;
-	}
-	
-	
-	public String toString(Integer id)
-	{
-		int flag=-1;
-		StringBuilder retorno = new StringBuilder();
-		Iterator<Entry<Integer,Users>> it = listMapUsers.entrySet().iterator();
-		while (it.hasNext()&&flag==-1) {
+
+	public Users getUser(String name) {
+		int flag = -1;
+		Users us = null;
+		Iterator<Entry<Integer, Users>> it = listMapUsers.entrySet().iterator();
+		while (it.hasNext() && flag == -1) {
 			Entry<Integer, Users> entry = it.next();
-			if (entry.getKey()== id) {
-				flag=0;
-				retorno.append(entry.getValue().toString()+"\t"+entry.getKey()+"\n");
+			if (entry.getValue().getName() == name) {
+				flag = 0;
+				us = entry.getValue();
+			}
+		}
+		return us;
+	}
+
+	public Integer getId(String name) {
+		int flag = -1;
+		int us = -1;
+		Iterator<Entry<Integer, Users>> it = listMapUsers.entrySet().iterator();
+		while (it.hasNext() && flag == -1) {
+			Entry<Integer, Users> entry = it.next();
+			if (entry.getValue().getName() == name) {
+				flag = 0;
+				us = entry.getKey();
+			}
+		}
+		return us;
+	}
+
+	public String toString(Integer id) {
+		int flag = -1;
+		StringBuilder retorno = new StringBuilder();
+		Iterator<Entry<Integer, Users>> it = listMapUsers.entrySet().iterator();
+		while (it.hasNext() && flag == -1) {
+			Entry<Integer, Users> entry = it.next();
+			if (entry.getKey() == id) {
+				flag = 0;
+				retorno.append(entry.getValue().toString() + "\t" + entry.getKey() + "\n");
 			}
 		}
 		return retorno.toString();
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,33 +1,54 @@
 package main;
 
 import List.Catalogue;
+import List.UserList;
 import Products.Clothing;
 import Products.Estate;
 import Products.HomeAppliances;
 import Products.Products;
 import Products.Vehicle;
+import types_users.Users;
 
 public class Main {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		UserList userList = new UserList();
+
+		// boolean autentificated = login(userList,"pepito","1234");
+
 		Catalogue<Products> catalogue = new Catalogue<Products>();
 		Products e = new HomeAppliances(1, "Cafetera", 20000, 80, 50, 20, 20);
 		Products v = new Vehicle(2, "Ford", 800000, 10, "Auto", true, 0);
 		Products i = new Estate(3, "Casa", 150000, 1, 4, true, "Mar del Plata");
 		Products c = new Clothing(4, "Remera", 1200, 40, 'M', "Verde", "Addidas");
-		
+
 		catalogue.add(e);
 		catalogue.add(v);
 		catalogue.add(i);
 		catalogue.add(c);
-		
+
 		catalogue.write();
-		
-		Catalogue<Products> c2 =  new Catalogue<Products>();
+
+		Catalogue<Products> c2 = new Catalogue<Products>();
 		c2 = catalogue.read();
-		
+
 		System.out.println(c2.getElement(0));
-		
+
 	}
 
+	public static boolean login(UserList userList, String username, String password) {
+		Users user = userList.getUser(username);
+		if (user == null) {
+			System.out.println("Usuario no encontrado");
+			return false;
+		} else {
+			if (user.getPassword() == password) {
+				System.out.println("Ha ingresado correctamente");
+				return true;
+			} else {
+				System.out.println("El password ingresado no coincide con el usuario");
+				return false;
+			}
+		}
+	}
 }
