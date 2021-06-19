4a753b2403e425c4185336afd5e6fe7d784e918d
MatiMorilla <matiasmorilla@hotmail.com>
2021-06-19 16:18:51 -0300

    error

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,21 +16,11 @@ import types_users.Users;
 public class Main {
 
 	public static void main(String[] args) {
-		//LogIn inicio = new LogIn();
+		LogIn inicio = new LogIn(); // ERROR EN EL LOGIN SOLO COMPARA CON EL ULTIMO USER INGRESADO
 		//Signup signup = new Signup();
 		//Principal principal = new Principal(new Client());
 		//Perfil perfil = new Perfil(new Client("Matias", "Morilla", "123", "matias@gmail.com", "Mar del Plata", "Patagones 834"));
 		//Perfil perfil = new Perfil(new Admin("Matias", "Morilla", "123", "matias@gmail.com"));
-
-		HashMap<Integer, Users> hashMap = UserUtiles.read();
-		Iterator<Entry<Integer, Users>> it = hashMap.entrySet().iterator();
-		
-		while(it.hasNext())
-		{
-			Entry<Integer, Users> entry = it.next();
-			System.out.println("A: " + entry.getValue().getName() + entry.getKey());
-
-		}
 		
 
 		
