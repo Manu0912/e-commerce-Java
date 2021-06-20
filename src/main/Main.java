bfd232b3977bf3ea186c2cd33e508ec19f5338c4
MatiMorilla <73543913+MatiMorilla@users.noreply.github.com>
2021-06-19 21:17:43 -0300

    arreglo error

--- src/main/Main.java
+++ src/main/Main.java
@@ -22,21 +22,7 @@ public class Main {
 		//Signup signup = new Signup();
 		//Principal principal = new Principal(new Client());
 		//Perfil perfil = new Perfil(new Client("Matias", "Morilla", "123", "matias@gmail.com", "Mar del Plata", "Patagones 834"));
-		//Perfil perfil = new Perfil(new Admin("Matias", "Morilla", "123", "matias@gmail.com"));
-
-		/*HashMap<Integer, Users> hashMap = UserUtiles.read();
-		System.out.println(hashMap.size());
-		Iterator<Entry<Integer, Users>> it = hashMap.entrySet().iterator();
-		
-		while(it.hasNext())
-		{
-			Entry<Integer, Users> entry = it.next();
-			int i = 0;
-			System.out.println(i+ 1);
-			System.out.println(entry.getKey() + "/"+ entry.getValue());
-
-		}*/
-		
+		//Perfil perfil = new Perfil(new Admin("Matias", "Morilla", "123", "matias@gmail.com"));		
 		
 	}
 	
